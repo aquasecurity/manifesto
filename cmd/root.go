2ba480a81289d63f0973b96163858f479689b930
Liz Rice <liz@lizrice.com>
2017-07-03 12:37:32 +0100

    Remove unused option

--- cmd/root.go
+++ cmd/root.go
@@ -53,7 +53,6 @@ func init() {
 	// RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.manifesto.yaml)")
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
